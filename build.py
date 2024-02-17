f9c0d10dc1c8a4cf3e215f69c8f03fe150a94d6b
Logan Dhillon <logandhi64@gmail.com>
2024-02-17 16:45:47 -0500

    Abandon abstract method

--- build.py
+++ build.py
@@ -1,45 +1,39 @@
-import importlib
-import inspect
 import os
-from cmdkit import Command, commands
-import textwrap
 
-def discover_commands():
-	# discover all modules
-	root = os.path.abspath("")
-	module_names = [
-		file[:-3] for file in os.listdir(root)
-		if file.endswith('.py') and not file.startswith('__')
-	]
+resolve_dist_path = lambda file: os.path.join("dist", file.split('.')[0])
 
-	# load the commands
-	for module_name in module_names:
-		module = importlib.import_module(f"{module_name}")
-		for _, body in inspect.getmembers(module):
-			if inspect.isclass(body) and issubclass(body, Command) and body != Command:
-				body()
+def wipe_dir(dir):
+	for filename in os.listdir(dir):
+		path = os.path.join(dir, filename)
+		try:
+			if os.path.isfile(path):
+				os.remove(path)
+				print(f"Deleted: {path}")
+		except Exception as e:
+			print(f"Failed to delete {path}: {e}")
+			exit(1)
+	print("Wiped " + dir)
 
-# FIXME: this only gets the contents of the exec() method body, thus, breaking the script if it has content outside of said method
-def create_command_files():
-	output_dir = "dist"
-	os.makedirs(output_dir, exist_ok=True)
-
-	for command_name, command_instance in commands.items():
-		file_path = os.path.join(output_dir, command_name)
-		with open(file_path, "w") as file:
-			file.write(process_exec_body(inspect.getsource(command_instance)))
-			
-def process_exec_body(input_string):
-	lines = input_string.split('\n', 1)
-	if len(lines) > 1:
-		_, stripped_lines = lines
-		stripped_lines = textwrap.dedent(stripped_lines)
-		stripped_lines = textwrap.dedent(stripped_lines)
-		return stripped_lines
-	else:
-		return ''
-
-			
 if __name__ == "__main__":
-	discover_commands()
-	create_command_files()
+	s = input("This will delete all files in dist, continue? [Y/n] ")
+	if s.lower() == "n":
+		exit(0)
+	wipe_dir("dist")
+
+	files = os.listdir("src")
+
+	print(f"Building {len(files)} script(s)")
+	for filename in files:
+		path = os.path.join("src", filename)
+
+		if not os.path.isfile(path):
+			continue
+
+		with open(path, 'r') as f:
+			contents = f.read()
+
+		out = resolve_dist_path(filename)
+		with open(out, "w") as f:
+			f.write(contents)
+
+		print(f"Created: {out}")
---
+++ src/testcmd.py
@@ -0,0 +1,7 @@
+print("test cmd")
+print("multi line print statements")
+
+def method(a):
+	return a + 2
+
+print(method(3))
