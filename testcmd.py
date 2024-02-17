6d132929fa9e00cfcf49b0794c117ad457e15046
Logan Dhillon <logandhi64@gmail.com>
2024-02-17 15:56:43 -0500

    Add indentation to testcmd

--- testcmd.py
+++ testcmd.py
@@ -3,3 +3,9 @@ from cmdkit import Command
 class testcmd(Command):
 	def exec(self):
 		print("test cmd")
+		print("multi line print statements")
+
+		def method(a):
+			return a + 2
+		
+		print(method(3))
